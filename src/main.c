50539a08310246a1db0e4841c391aa8e9dd0d1ac
bi3mer <colan@comcast.net>
2025-11-09 21:38:12 -0500

    added a laser pionter and a help menu

--- src/main.c
+++ src/main.c
@@ -46,6 +46,8 @@ int main(int argc, char *argv[])
     // Start raylib application
     int slide_index = 0; // temp
     int num_slides = 30; // temp
+    bool show_cursor = true;
+    bool show_help = false;
 
     const int default_width = 1080;
     const int default_height = 720;
@@ -68,6 +70,20 @@ int main(int argc, char *argv[])
             slide_index = min_i(num_slides - 1, slide_index + 1);
         }
 
+        if (IsKeyPressed(KEY_H))
+        {
+            show_help = !show_help;
+        }
+
+        if (IsKeyPressed(KEY_S))
+        {
+            show_cursor = !show_cursor;
+            if (show_cursor)
+                ShowCursor();
+            else
+                HideCursor();
+        }
+
         RenderTexture2D texture =
             LoadRenderTexture(default_width, default_height);
         BeginDrawing();
@@ -75,13 +91,14 @@ int main(int argc, char *argv[])
         const int screen_width = GetScreenWidth();
         const int screen_height = GetScreenHeight();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(theme.background);
 
         BeginTextureMode(texture);
 
         DrawText("Congrats! You created your first window!", 190, 200,
                  theme.text_size, LIGHTGRAY);
 
+        /////////// slide numbers ///////////
         if (theme.show_slide_numbers)
         {
             char buffer[10];
@@ -96,7 +113,56 @@ int main(int argc, char *argv[])
                 GetFontDefault(), buffer, theme.slide_number_size, 0);
             DrawText(buffer, default_width - 1.3 * text_dimensions.x,
                      default_height - 1.3 * text_dimensions.y,
-                     theme.slide_number_size, RED);
+                     theme.slide_number_size, theme.text);
+        }
+
+        /////////// help menu ///////////
+        if (show_help)
+        {
+            const int help_title_size = 30;
+            const int help_font_size = 25;
+            const Vector2 top_left = {.x = 0.1 * default_width,
+                                      .y = 0.1 * default_height};
+            const Vector2 dimensions = {
+                .x = default_width * 0.8,
+                .y = default_height * 0.8,
+            };
+
+            DrawRectangleV(top_left, dimensions, LIGHTGRAY);
+
+            // title
+            const int tile_width = MeasureText("Help Menu", help_font_size);
+            DrawText("Help Menu",
+                     top_left.x + dimensions.x / 2 - tile_width / 2,
+                     top_left.x - 20, help_title_size, RED);
+
+            // general directions
+            int y_mod = 80;
+            DrawText("- A, H, or left arrow to go left", top_left.x + 10,
+                     top_left.y + y_mod, help_font_size, RED);
+
+            y_mod += 30;
+            DrawText("- D, L, or right arrow to go right", top_left.x + 10,
+                     top_left.y + y_mod, help_font_size, RED);
+
+            y_mod += 30;
+            DrawText("- H to show the help menu (this)", top_left.x + 10,
+                     top_left.y + y_mod, help_font_size, RED);
+
+            y_mod += 30;
+            DrawText("- S to show laser pointer", top_left.x + 10,
+                     top_left.y + y_mod, help_font_size, RED);
+        }
+
+        /////////// red laser cursor ///////////
+        if (!show_cursor)
+        {
+            HideCursor();
+            Vector2 mouse = GetMousePosition();
+            mouse.x = (mouse.x / screen_width) * default_width;
+            mouse.y = (mouse.y / screen_height) * default_height;
+
+            DrawCircle(mouse.x, mouse.y, 5, RED);
         }
 
         EndTextureMode();
